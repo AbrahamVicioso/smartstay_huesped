edb7ca21880692ac19611534a0dda74f672c225d
Abraham <abrahanvicioso@gmail.com>
2026-05-04 08:46:19 -0400

    fix(api_config): update API URLs to point to local development server

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,12 +1,16 @@
 class ApiConfig {
-  static const String baseUrl = 'https://api.smartstay.es';
+  // static const String baseUrl = 'https://api.smartstay.es';
+  static const String baseUrl = 'http://10.0.0.33:5019';
 
   static const String authBaseUrl = '$baseUrl/api/auth';
   static const String reservasBaseUrl = '$baseUrl/api/reserva';
   static const String usuariosBaseUrl = '$baseUrl/api/user';
 
+  // static const String pushConfigUrl =
+  //     'https://api.smartstay.es/api/notifications/push-config';
+
   static const String pushConfigUrl =
-      'https://smartstay.es/api/notifications/push-config';
+    'http://10.0.0.33:5019/api/notifications/push-config';
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration receiveTimeout = Duration(seconds: 30);
